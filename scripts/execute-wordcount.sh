fe05faaafccbf0934c084acaf19975f04a2cf045
Marco Rosa <marcor165@hotmail.it>
2019-02-14 12:01:46 +0100

    fix execute-wordcount.sh script

--- scripts/execute-wordcount.sh
+++ scripts/execute-wordcount.sh
@@ -6,4 +6,4 @@ sudo -u cloudgene hadoop fs -mkdir input
 # Add some data to the input directory
 sudo -u cloudgene hadoop fs -put /etc/hadoop/conf/*.xml input
 # Execute Wordcount
-$ hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar grep input output23 'dfs[a-z.]+'
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar grep input output23 'dfs[a-z.]+'
